e205ecce84016b42bfa9bbabb83e60441219e13e
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-09-10 14:08:24 +0200

    [*] Fix tools interface

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const BadgeCoverageUpdater = require('./services/badge-coverage-updater');
 const ReleaseCreator = require('./services/release-creator');
 const ReleaseNoteCreator = require('./services/release-note-creator');
 
+function ReleaseManager(slackToken, releaseIcon, options) {
+  this.create = () => new ReleaseCreator(process.argv, options)
+    .perform()
+    .then(() => new ReleaseNoteCreator(slackToken, releaseIcon, options).perform());
+}
+
+function CoverageManager() {
+  this.updateBadge = async () => new BadgeCoverageUpdater().perform();
+}
+
 module.exports = {
-  ReleaseManager(slackToken, releaseIcon, options) {
-    this.create = () => new ReleaseCreator(process.argv, options)
-      .perform()
-      .then(() => new ReleaseNoteCreator(slackToken, releaseIcon, options).perform());
-  },
-  CoverageManager() {
-    this.updateBadge = async () => new BadgeCoverageUpdater().perform();
-  },
+  ReleaseManager,
+  CoverageManager,
 };
